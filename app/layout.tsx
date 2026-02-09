394d8124c4d6d1d60e71345a1d6aa2905c83e0c4
dxdleady <dxdleady@users.noreply.github.com>
2026-02-09 23:23:37 +0700

    feat: base metatag

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ export async function generateMetadata(): Promise<Metadata> {
     title: minikitConfig.miniapp.name,
     description: minikitConfig.miniapp.description,
     other: {
+      "base:app_id": "698a09ae73cda529e5cd68cb",
       "fc:miniapp": JSON.stringify({
         version: minikitConfig.miniapp.version,
         imageUrl: minikitConfig.miniapp.heroImageUrl,
